5739f9bcc7c5722f339da1eec546aa81d53a248a
Abi Ishola <abiodun.ve@gmail.com>
2018-02-15 16:52:20 +0000

    Valtech Tests

---
+++ ValtechTests/Helper/Context.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ValtechTests.Helper
+{
+    public class Context
+    {
+        public IWebDriver Driver;
+        private string BaseUrl { get; } = "https://www.valtech.com";
+
+        public void BrowserSetup()
+        {
+            var chromeDriverProcesses = Process.GetProcessesByName("ChromeDriver");
+
+            foreach (var chromeDriverProcess in chromeDriverProcesses)
+            {
+                try
+                {
+                    chromeDriverProcess.Kill();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            Driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl(BaseUrl);
+            Driver.Manage().Window.Maximize();
+        }
+
+        public void CloseBrowser()
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+
+    }
+    
+}
